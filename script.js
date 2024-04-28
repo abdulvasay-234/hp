71d9a25c5ef6f1f0b8e353368daf44f06fd1548f
Afnan Abdul Vasay <afnanabdulvasay234@gmail.com>
2024-04-29 01:15:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navContainer = document.getElementById('navContainer');
-    if (window.scrollY > 50) { // Adjust the value (50) to change when to change the color
-      navContainer.classList.add('scrolled');
-    } else {
-      navContainer.classList.remove('scrolled');
-    }
-  });
-  
